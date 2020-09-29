9d7ba9d6c19a356f08c4932daccb91566c679aaf
porosh2002 <jamilkashem2002@gmail.com>
2020-09-30 04:18:54 +0600

    a

--- Server.js
+++ Server.js
@@ -76,7 +76,6 @@ var corsOptions = {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy());
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
